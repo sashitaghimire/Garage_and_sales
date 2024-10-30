47bca77cf6bf755e7adb113e71ef64eb7093ace2
sashitaghimire <ghimiresashita@gmail.com>
2024-10-30 19:23:52 +0545

    scroll issue fixes

--- src/ImageCarousel/ImageCarousel.jsx
+++ src/ImageCarousel/ImageCarousel.jsx
@@ -10,63 +10,44 @@ import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 export default function ImageCarousel() {
   const slides = [
-    {
-      url: intro1,
-      title: "Mobile Screen 1",
-    },
-    {
-      url: intro2,
-      title: "Mobile Screen 2",
-    },
-    {
-      url: intro3,
-      title: "Mobile Screen 3",
-    },
-    {
-      url: intro4,
-      title: "Mobile Screen 4",
-    },
-    {
-      url: intro5,
-      title: "Mobile Screen 5",
-    },
+    { url: intro1, title: "Mobile Screen 1" },
+    { url: intro2, title: "Mobile Screen 2" },
+    { url: intro3, title: "Mobile Screen 3" },
+    { url: intro4, title: "Mobile Screen 4" },
+    { url: intro5, title: "Mobile Screen 5" },
+    { url: intro2, title: "Mobile Screen 2" },
   ];
+
+  // Preload images to reduce blinking
+  slides.forEach((slide) => {
+    const img = new Image();
+    img.src = slide.url;
+  });
 
   return (
     <div className="container">
       <Swiper
         breakpoints={{
-          640: {
-            slidesPerView: 1.5,
-          },
-          768: {
-            slidesPerView: 3.5,
-          },
-          1024: {
-            slidesPerView: 4.5,
-          },
+          640: { slidesPerView: 1.5 },
+          768: { slidesPerView: 3.5 },
+          1024: { slidesPerView: 5 },
         }}
-        freeMode={true}
         spaceBetween={30}
         className="swiper-container"
-        pagination={{
-          clickable: true,
-        }}
+        pagination={{ clickable: true }}
         autoplay={{
-          delay: 0,
+          delay: 1000, // Set to 2000ms for smoother transitions
           disableOnInteraction: false,
         }}
-        speed={5000}
+        speed={2000} // Reduced speed for smoother transitions
         navigation={true}
         modules={[Pagination, Navigation, Autoplay]}
         loop={true}
         grabCursor={true}
-        loopAddBlankSlides={true}
-        loopPreventsSliding={true}
       >
         {slides.map((slide, index) => (
-          <SwiperSlide key={index + 1}>
-            <img src={slide.url} alt={slide.title} />
+          <SwiperSlide key={index}>
+            <img src={slide.url} alt={slide.title} className="w-full" />
           </SwiperSlide>
         ))}
       </Swiper>
