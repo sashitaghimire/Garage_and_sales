1e8be4bbdfe5d39c8213684ee0db7e50dfaa6f4c
sashitaghimire <ghimiresashita@gmail.com>
2024-12-24 21:39:14 +0545

    feat: add video link in download app

--- src/DownloadApp/DownloadApp.jsx
+++ src/DownloadApp/DownloadApp.jsx
@@ -1,12 +1,13 @@
 import play_store from "../assets/play_store.png";
 import app_store from "../assets/app_store.png";
 import qr_code from "../assets/qr_code.png";
+import find_garage from "../assets/find_garage.mp4";
 
 const DownloadApp = () => {
   return (
     <div className="container mx-auto">
       <div className=" bg-brand-light-orange p-6 md:p-10 rounded-lg px-4">
-        <div className=" container flex flex-col md:flex-row md:items-center md:justify-between gap-8 md:gap-4">
+        <div className=" container flex flex-col md:flex-col md:items-center md:justify-center gap-8 md:gap-4">
           {/* Left section with text and store buttons */}
           <div className="text-center md:text-left">
             <div className="text-4xl md:text-4xl sm:text-xl font-bold text-brand-brown mb-2">
@@ -16,7 +17,7 @@ const DownloadApp = () => {
               Buy and sell your items Easily, Safely, and Quickly.
             </p>
             {/* Store buttons container */}
-            <div className="flex flex-col sm:flex-row items-center sm:items-start gap-4 sm:gap-4">
+            <div className="flex flex-col sm:flex-row items-center sm:items-center justify-center gap-4 sm:gap-4">
               <a
                 href="https://play.google.com/store/apps/details?id=com.sale.garageyard&hl=en_US&gl=US"
                 className="transform hover:scale-105 transition-transform"
@@ -41,6 +42,16 @@ const DownloadApp = () => {
               </a>
             </div>
           </div>
+          <video
+            controls
+            autoPlay
+            style={{
+              borderRadius: "10px",
+              marginTop: "20px",
+            }}
+          >
+            <source src={find_garage} type="video/mp4" />
+          </video>
 
           {/* Right section with QR code */}
           <div className="flex flex-col items-center justify-center">
